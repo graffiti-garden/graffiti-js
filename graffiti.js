28b3586fc4a1e5c1f5bd7f7865a33d6d34dfd6a2
anonymous <anonymous@example.com>
2022-09-21 12:46:08 -0400

    anonymized

--- graffiti.js
+++ graffiti.js
@@ -2,7 +2,7 @@ import Auth from './auth.js'
 
 export default class {
 
-  constructor(graffitiURL="https://graffiti.csail.mit.edu") {
+  constructor(graffitiURL="https://graffiti.garden") {
     this.graffitiURL = graffitiURL
     this.open = false
     this.subscriptionData = {}
